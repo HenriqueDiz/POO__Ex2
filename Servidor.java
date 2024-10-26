8cb0eb5a7eccff62d18dba3ce638ea98a9b822d9
Tomás Gonçalves <166638083+bauertommy4@users.noreply.github.com>
2024-10-26 04:49:48 +0100

    Update Servidor.java
    p/ ter lim sup do CPU

--- Servidor.java
+++ Servidor.java
@@ -2,9 +2,8 @@ public class Servidor extends Computador{
 
 	public Servidor(int id){
 		super(id, "x64","Cloud");
-		generateSpecs(7, 9, 10, 13, 3.0);
+		generateSpecs(7, 9, 10, 13, 3.0, 4.0);
 	}
-
 	public double calcularConsumo(){
 		return 80 * this.cpu;
 	}
