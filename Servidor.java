050860336639178aa4d1a51c32f8617ece4b5543
Tomás Gonçalves <166638083+bauertommy4@users.noreply.github.com>
2024-10-30 16:56:17 +0000

    javadoc servidor

--- Servidor.java
+++ Servidor.java
@@ -1,10 +1,25 @@
-public class Servidor extends Computador{
+/**
+ * Classe servidor
+ * arquitetura x64 e nivel Cloud
+ */
+public class Servidor extends Computador {
 
-	public Servidor(int id){
-		super(id, "x64","Cloud");
-		generateSpecs(7, 9, 10, 14, 3.0);
-	}
-	public double calcularConsumo(){
-		return 80 * this.cpu;
-	}
+    /**
+     * construtor da classe Servidor
+     *
+     * @param id id de cada servidor
+     */
+    public Servidor(int id) {
+        super(id, "x64", "Cloud");
+        generateSpecs(7, 9, 10, 14, 3.0);
+    }
+
+    /**
+     * consumo energia do servidor
+     *
+     * @return consumo em watts
+     */
+    public double calcularConsumo() {
+        return 80 * this.cpu;
+    }
 }
