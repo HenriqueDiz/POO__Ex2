c1c4a008a3caec6ebc8ba7b8fb858dac278fb017
Tomás Gonçalves <166638083+bauertommy4@users.noreply.github.com>
2024-10-30 16:54:32 +0000

    javadoc lab

--- NCSLab.java
+++ NCSLab.java
@@ -1,12 +1,26 @@
 import java.util.ArrayList;
 
+/**
+ * Classe do lab 
+ * contem computadores
+ * inicializa stock
+ * calcula também consumo energetico
+ */
 public class NCSLab {
     private ArrayList<Computador> stock;
-    
+
+    /**
+     * construtor do lab 
+     * stock inicial de computadores
+     */
     public NCSLab() {
-        stock = new ArrayList<>(15);  // Stock inicial de 15 computadores
+        stock = new ArrayList<>(15); // stock inicial de 15 computadores
     }
 
+    /**
+     * gerar stock do lab
+     * computadores de diferentes tipos
+     */
     public void initializeStock() {
         int id = 1;
         for (int i = 0; i < 5; i++) {
@@ -16,23 +30,32 @@ public class NCSLab {
         }
     }
 
-    public void imprimirTodosComputadores(){
-        for(Computador c : stock){
+    /**
+     * print specs de todos os computadores nesse stock
+     */
+    public void imprimirTodosComputadores() {
+        for (Computador c : stock) {
             c.imprimirComputador();
         }
     }
 
+    /**
+     * print specs de todos os computadores x64
+     */
     public void imprimirX64() {
-        for(Computador c : stock){
-            if(c.arquitetura.equals("x64")){
+        for (Computador c : stock) {
+            if (c.getArquitetura().equals("x64")) {
                 c.imprimirComputador();
             }
         }
     }
 
-    public void calcularConsumo(){
-        for(Computador c : stock){
-            System.out.printf("ID %d - Consumo = %.2f W\n\n", c.id, c.calcularConsumo());
+    /**
+     * calculo e print do consumo energetico de todos os computadores nesse stock
+     */
+    public void calcularConsumo() {
+        for (Computador c : stock) {
+            System.out.printf("ID %d - Consumo = %.2f W\n\n", c.getId(), c.calcularConsumo());
         }
-    }           
+    }
 }
